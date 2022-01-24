0b2268cd639014fc1fbe139f16e562b5cc28f35f
Lukasz Okraszewski <work@lokraszewski.com>
2022-01-24 20:00:05 +0000

    feat: Added bat package and local/bin symlink

--- roles/zsh/files/environment.zsh
+++ roles/zsh/files/environment.zsh
@@ -7,5 +7,6 @@
 # Misc. Environment Variables
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 CARGO_PATH=$HOME/.cargo/bin
-export PATH="$CARGO_PATH:$PATH"
+LOCAL_PATH=$HOME/.local/bin
+export PATH="$CARGO_PATH:$LOCAL_PATH:$PATH"
 export TERM=xterm-256color # Fixes highlight style in tmux

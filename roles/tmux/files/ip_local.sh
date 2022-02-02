1d75cbfed481b428ba3ca60451e1695b2795b65a
Lukasz Okraszewski <work@lokraszewski.com>
2022-02-02 21:47:19 +0000

    fix(tmux): Fix bad ip awk on some systems

--- roles/tmux/files/ip_local.sh
+++ roles/tmux/files/ip_local.sh
@@ -7,7 +7,7 @@ main()
     # Only Linux for now since I am lazy, also I don't own a Mac.
     Linux)
       # hostname is unreliable since we cannot make assumptions about the order of addresses
-      ip route get 1 | awk '{print $(NF-2);exit}'
+      ip route get 1 | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -n1
       ;;
 
     *)

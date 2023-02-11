fd8218a02dd74fcaf9170d655b863ec7b815011e
Lukasz Okraszewski <work@lokraszewski.com>
2023-02-11 14:05:44 +0000

    feat(go): add go launcher g

--- roles/zsh/files/environment.zsh
+++ roles/zsh/files/environment.zsh
@@ -8,5 +8,8 @@
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 CARGO_PATH=$HOME/.cargo/bin
 LOCAL_PATH=$HOME/.local/bin
-export PATH="$CARGO_PATH:$LOCAL_PATH:$PATH"
+export GOROOT=$HOME/.go
+export GOPATH=$HOME/go
+
+export PATH="$GOPATH/bin:$CARGO_PATH:$LOCAL_PATH:$PATH"
 export TERM=xterm-256color # Fixes highlight style in tmux

2cc9950683de64b1222b8d098b3ba46ad8de285d
Lukasz Okraszewski <work@lokraszewski.com>
2023-02-11 15:02:28 +0000

    feat(pyenv): add pyenv launcher

--- roles/zsh/files/environment.zsh
+++ roles/zsh/files/environment.zsh
@@ -10,6 +10,10 @@ CARGO_PATH=$HOME/.cargo/bin
 LOCAL_PATH=$HOME/.local/bin
 export GOROOT=$HOME/.go
 export GOPATH=$HOME/go
+export PYENV_ROOT=$HOME/.pyenv
 
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 export PATH="$GOPATH/bin:$CARGO_PATH:$LOCAL_PATH:$PATH"
 export TERM=xterm-256color # Fixes highlight style in tmux
+
+eval "$(pyenv init --path)"
